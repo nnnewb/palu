3cc21b209a834d43b7b167d831fc0bafeb457ac0
weak_ptr <weak_ptr@outlook.com>
2021-09-03 17:00:02 +0800

    略微优化transpile，清洁输出

--- palu/transpile/transpiler.py
+++ palu/transpile/transpiler.py
@@ -27,46 +27,45 @@ class Transpiler(metaclass=ABCMeta):
     def __init__(self) -> None:
         super().__init__()
 
-    def transpile(self, node: ASTNode):
+    def transpile(self, node: ASTNode, semi=True):
+        semi_sym = ';' if semi else ''
         if isinstance(node, DeclareStatement):
-            return self.transpile_declare_stmt(node)
-        elif isinstance(node, ExternalStatement):
-            return self.transpile_external_stmt(node)
+            return self.transpile_declare_stmt(node) + semi_sym
         elif isinstance(node, WhileLoop):
-            return self.transpile_while_stmt(node)
+            return self.transpile_while_stmt(node) 
         elif isinstance(node, IfBranch):
-            return self.transpile_if_stmt(node)
+            return self.transpile_if_stmt(node) 
         elif isinstance(node, ReturnStatement):
-            return self.transpile_return_stmt(node)
+            return self.transpile_return_stmt(node) + semi_sym
         elif isinstance(node, Func):
-            return self.transpile_func_stmt(node)
+            return self.transpile_func_stmt(node) 
         elif isinstance(node, TypeAliasStatement):
-            return self.transpile_type_alias_stmt(node)
+            return self.transpile_type_alias_stmt(node) + semi_sym
         elif isinstance(node, IdentExpr):
-            return self.transpile_ident_expr(node)
+            return self.transpile_ident_expr(node) + semi_sym
         elif isinstance(node, BinaryExpr):
-            return self.transpile_binary_expr(node)
+            return self.transpile_binary_expr(node) + semi_sym
         elif isinstance(node, UnaryExpr):
-            return self.transpile_unary_expr(node)
+            return self.transpile_unary_expr(node) + semi_sym
         elif isinstance(node, ConditionExpr):
-            return self.transpile_condition_expr(node)
+            return self.transpile_condition_expr(node) + semi_sym
         elif isinstance(node, CallExpr):
-            return self.transpile_call_expr(node)
+            return self.transpile_call_expr(node) + semi_sym
         elif isinstance(node, ParenthesizedExpr):
-            return self.transpile_parenthesized_expr(node)
+            return self.transpile_parenthesized_expr(node) + semi_sym
         elif isinstance(node, NumberLiteral):
-            return self.transpile_number_literal(node)
+            return self.transpile_number_literal(node) + semi_sym
         elif isinstance(node, StringLiteral):
-            return self.transpile_string_literal(node)
+            return self.transpile_string_literal(node) + semi_sym
         elif isinstance(node, BooleanLiteral):
-            return self.transpile_boolean_literal(node)
+            return self.transpile_boolean_literal(node) + semi_sym
         elif isinstance(node, NullLiteral):
-            return self.transpile_null_literal(node)
+            return self.transpile_null_literal(node) + semi_sym
         else:
             raise Exception(f'unexpected ast node {node}')
 
     def transpile_declare_stmt(self, node: DeclareStatement):
-        return f'{self.transpile_ident_expr(node.typed_ident.typing)} {node.typed_ident.ident};'
+        return f'{self.transpile_ident_expr(node.typed_ident.typing)} {node.typed_ident.ident}'
 
     def transpile_external_stmt(self, node):
         raise NotImplementedError()
@@ -79,12 +78,12 @@ class Transpiler(metaclass=ABCMeta):
         body = '\n'.join(statements)
 
         return dedent(f'''\
-        while({self.transpile(node.condition)}) {{
+        while({self.transpile(node.condition, False)}) {{
         {indent(body, "    ")}
         }}''')
 
     def transpile_if_stmt(self, node: IfBranch):
-        condition = self.transpile(node.condition)
+        condition = self.transpile(node.condition, False)
 
         consequence = []
         for stmt in node.consequence:
@@ -97,17 +96,20 @@ class Transpiler(metaclass=ABCMeta):
 
         alternative_body = '\n'.join(alternative)
         consequence_body = '\n'.join(consequence)
+        if alternative:
+            alternative_body = f'''\
+            else {{
+            {indent(alternative_body,"    ")}
+            }}'''
 
         return dedent(f'''\
         if({condition}) {{
         {indent(consequence_body,"    ")}
-        }} else {{
-        {indent(alternative_body,"    ")}
-        }}''')
+        }} ''' + alternative_body)
 
     def transpile_return_stmt(self, node: ReturnStatement):
-        returns = self.transpile(node.expr)
-        return f'return {returns};'
+        returns = self.transpile(node.expr, False)
+        return f'return {returns}'
 
     def transpile_func_stmt(self, node: Func):
         params = []
@@ -122,7 +124,7 @@ class Transpiler(metaclass=ABCMeta):
         func_body = '\n'.join(statements)
 
         return dedent(f'''\
-        {self.transpile(node.returns)} {node.func_name}({",".join(params)}) {{
+        {self.transpile(node.returns, False)} {node.func_name}({",".join(params)}) {{
         {indent(func_body, "    ")}
         }}''')
 
@@ -137,29 +139,29 @@ class Transpiler(metaclass=ABCMeta):
         return '.'.join(node.ident)
 
     def transpile_binary_expr(self, node: BinaryExpr):
-        return f'({self.transpile(node.left)} {node.op.value} {self.transpile(node.right)})'
+        return f'({self.transpile(node.left, False)} {node.op.value} {self.transpile(node.right, False)})'
 
     def transpile_unary_expr(self, node: UnaryExpr):
-        return f'{node.op.value}{self.transpile(node.expr)}'
+        return f'{node.op.value}{self.transpile(node.expr, False)}'
 
     def transpile_condition_expr(self, node: ConditionExpr):
-        return f'({self.transpile(node.condition)}) ? ({self.transpile(node.consequence)}) : ({self.transpile(node.alternative)})'
+        return f'({self.transpile(node.condition, False)}) ? ({self.transpile(node.consequence, False)}) : ({self.transpile(node.alternative, False)})'
 
     def transpile_call_expr(self, node: CallExpr):
         args = []
         for arg in node.args:
-            args.append(self.transpile(arg))
+            args.append(self.transpile(arg, False))
 
-        return f'{self.transpile(node.ident)}({",".join(args)})'
+        return f'{self.transpile(node.ident, False)}({",".join(args)})'
 
     def transpile_parenthesized_expr(self, node: ParenthesizedExpr):
-        return f'({self.transpile(node.expr)})'
+        return f'({self.transpile(node.expr, False)})'
 
     def transpile_number_literal(self, node: NumberLiteral):
         return f'{node.value}'
 
     def transpile_string_literal(self, node: StringLiteral):
-        return f'"{node.value}"'
+        return f'{node.value}'
 
     def transpile_boolean_literal(self, node: BooleanLiteral):
         return f'{node.value}'.upper()
